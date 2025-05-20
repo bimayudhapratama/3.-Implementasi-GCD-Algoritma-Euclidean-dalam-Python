ca263ba17301092d673de8596c81cd960078c1a9
BIMA YUDHA PRATAMA <117697410+bimayudhapratama@users.noreply.github.com>
2025-05-20 20:11:47 +0700

    gcm.py

--- gcm.py
+++ gcm.py
@@ -1,17 +1,11 @@
 def gcd_iterative(a, b):
-    """
-    Menghitung GCD (FPB) dari dua bilangan bulat menggunakan algoritma Euclidean iteratif.
 
-    Args:
-        a: Bilangan bulat pertama.
-        b: Bilangan bulat kedua.
-
-    Returns:
-        GCD dari a dan b.
-    """
-    while b:  # Selama b bukan nol
-        a, b = b, a % b  # Ganti a dengan b, dan b dengan sisa bagi a % b
+    while b:
+        a, b = b, a % b
         print(f"gcd({a},{b}) = gcd({b}, {a} mod {b})")
     return a
 
-print(f"GCD(101, 103) = {gcd_iterative(101, 103)}") # Output: GCD(101, 103) = 1 (bilangan prima relatif)
+bil1 = int(input("Masukan Nilai Bilangan Pertama (a) : ")
+bil2 = int(input("Masukan Nilai Bilangan Kedua (b) : ")
+           
+print(f"GCD({bil1},{bil2} ) = {gcd_iterative(bil1, bil2)}")
